6f98888ec7d18da4b41b3e8e0f1ab428bff761f5
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-26 18:56:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -334,5 +334,5 @@ def player_with_longest_name
      away_counter += 1
   end
 
-  all_names.max_by(&:length)
+  all_names.max_by(&:length) #// This will select the longest string in the array
 end
